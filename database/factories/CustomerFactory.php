92c045eb549221300b99262a2e1b12cc7bb257b9
Joe Gitonga <thekiharani@gmail.com>
2021-11-08 14:41:06 +0300

    init

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone_number' => $this->faker->unique()->e164PhoneNumber(),
+            'last_contacted' => now()->addMinutes(random_int(0, 720)),
+            'is_active' => $this->faker->randomElement([0, 1])
+        ];
+    }
+}
